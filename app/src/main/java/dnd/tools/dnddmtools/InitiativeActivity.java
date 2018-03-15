a1a336a8f5b6f2d9a033e2cce8f81f79cc0a7f43
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2018-03-15 10:00:01 +0100

    add activity for the initiativeTracker

---
+++ app/src/main/java/dnd/tools/dnddmtools/InitiativeActivity.java
@@ -0,0 +1,13 @@
+package dnd.tools.dnddmtools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitiativeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initiative);
+    }
+}
