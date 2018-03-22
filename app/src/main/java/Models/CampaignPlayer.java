f5b7552c3fba1fc9af97da03ccf85e90da8ab204
maxhennen <m.hennen@student.fontys.nl>
2018-03-22 15:13:47 +0100

    playerstats

--- app/src/main/java/Models/CampaignPlayer.java
+++ app/src/main/java/Models/CampaignPlayer.java
@@ -1,5 +1,8 @@
 package Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,10 +11,31 @@ import java.util.Map;
  * Created by maxhe on 15-3-2018.
  */
 
-public class CampaignPlayer {
+public class CampaignPlayer implements Parcelable{
     private String id;
     private List<Skill> skillList;
     private String name;
+
+    public CampaignPlayer(){
+
+    }
+
+    protected CampaignPlayer(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+    }
+
+    public static final Creator<CampaignPlayer> CREATOR = new Creator<CampaignPlayer>() {
+        @Override
+        public CampaignPlayer createFromParcel(Parcel in) {
+            return new CampaignPlayer(in);
+        }
+
+        @Override
+        public CampaignPlayer[] newArray(int size) {
+            return new CampaignPlayer[size];
+        }
+    };
 
     public List<Skill> getSkillList() {
         return skillList;
@@ -40,5 +64,16 @@ public class CampaignPlayer {
     @Override
     public String toString(){
         return name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(name);
     }
 }
